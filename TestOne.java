e8e90588e25d07343ea7f60fa5292386e50cdad4
onlykdq <743295645@qq.com>
2017-04-28 19:54:46 +0800

    Add files via upload

---
+++ TestOne.java
@@ -0,0 +1,13 @@
+
+public abstract class TestOne {
+
+	public static void main(String[] args) {
+		double sum=0;
+		for(int i=1;i<=100;i++){
+			sum=sum+(1.0/i);
+		}
+			System.out.println(sum);
+			
+	}
+
+}
